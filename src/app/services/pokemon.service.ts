bbfa0e8e1356ecd5527cef0c7c1f8e658255a95a
Maria Clara Macedo Cantizano <macedocantizano@gmail.com>
2022-05-19 11:06:44 +0000

    postando pokedex2

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private url = 'https://pokeapi.co/api/v2/';
+  constructor() { }
+}
