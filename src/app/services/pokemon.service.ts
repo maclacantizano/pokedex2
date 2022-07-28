6aa6c468b09d287076b32b3262e081d2870800fe
maclacantizano <macedocantizano@gmail.com>
2022-07-28 15:07:15 -0300

    1 file modified

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -9,10 +9,10 @@ export class PokemonService {
   public listaPokemon = [];
 
   private url = 'https://pokeapi.co/api/v2/';
-
-  constructor(private http: HttpClient) { }
   
+  constructor(private http: HttpClient) { }
+
   buscarTodosPokemon() {
-    return this.http.get(this.url);
+    return this.http.get(`${this.url}pokemon`);
   }
 }
